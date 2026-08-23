9c79703eeadc929eb9ceea78d11b70cc9638de1c
Timothy Luk <timothyluk.wt@gmail.com>
2026-08-23 23:09:51 +0800

    added profile picture

--- frontend/src/Pages/Dashboard/Dashboard.tsx
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -1,23 +1,31 @@
 // Import Dependencies
 
-
 // Import files
 import './Dashboard.css';
+
 import { getCaregiverInfo } from '../../Context/CaregiverContext';
 
 function Dashboard(){
-    const { activeCaregiverFirstName, loading } = getCaregiverInfo();
+    const { activeCaregiver, loading } = getCaregiverInfo();
 
+    const defaultProfilePic = '/Assets/profile_pic_default.jpg';
+    const pictureUrl = (loading ? defaultProfilePic : activeCaregiver?.profile_picture_url) ?? defaultProfilePic;
+    console.log(pictureUrl);
+
+    const profilePictureStyle = {
+        backgroundImage: `url(${pictureUrl})`,
+    };
+    
     return(
         <div className="Dashboard">
             {/* Welcome Banner */}
             <div className="WelcomeHeader">
                 <div>
                     <p>Good morning,</p>
-                    <h1>{loading ? '' : `${activeCaregiverFirstName ?? 'User'}!`}</h1>
+                    <h1>{loading ? '' : `${activeCaregiver?.profile.first_name ?? 'User'}.`}</h1>
                 </div>
-                <div className="UserProfilePic">
-
+                <div className="ProfilePicContainer">
+                    <div className="UserProfilePic" style={profilePictureStyle}></div>
                 </div>
             </div>
 
